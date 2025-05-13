1c3822d58837e8c22c7e3e14e6e12075ccedbc9b
braedenwb <braedenwboyce@gmail.com>
2025-05-13 06:06:22 -0500

    more folder structure changes

--- src/studytool/Controller.java
+++ src/studytool/Controller.java
@@ -1,5 +1,6 @@
 package studytool;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
--- src/studytool/Main.java
+++ src/studytool/Main.java
@@ -1,5 +1,6 @@
 package studytool;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
